95663e31e84f4dc8032cf072f598b3dd9d350480
ckw82076 <ckw82076@HA-STEMCART-33>
2024-09-17 10:08:22 -0400

    Add project files.

---
+++ CSC 221 Initial Program/CSC 221 Initial Program.cpp
@@ -0,0 +1,8 @@
+// CSC 221 Initial Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Chapter 2 Programs\n"; 
